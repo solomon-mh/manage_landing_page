aba19673a3210272366e0359624661cab8ef863b
SolomonMuhyeY <solomon.muhye.wd@gmail.com>
2023-05-22 09:03:21 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brightRed: "hsl(12,88%,50%)",
+        brightRedLight: "hsl(12,88%,69%)",
+        brightRedSupLight: "hsl(12,88%,95%)",
+        darkBlue: "hsl(12,88%,50%)",
+        darkGrayishBlue: "hsl(227,12%,61%)",
+        veryDarkBlue: "hsl(233,12%,13%)",
+        veryPaleRed: "hsl(13,100%,96%)",
+        veryLightGray: "hsl(0,0%,98%)",
+      },
+    },
+  },
+  plugins: [],
+};
